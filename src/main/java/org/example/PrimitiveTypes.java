21afa6f0c6bf7e0c2c1316a3a64ef4849d91c084
Luis Aranguren <luisarangurnn@gmail.com>
2024-10-28 06:56:57 -0300

    feat: PrimitiveTypesCompletes

--- src/main/java/org/example/PrimitiveTypes.java
+++ src/main/java/org/example/PrimitiveTypes.java
@@ -6,7 +6,25 @@ public class PrimitiveTypes {
 
         //escribir un boolean, asignarlo y mostrar en consola
         boolean isActive = true;
-
         System.out.println(isActive);
+        //Numeros Enteros()
+        byte primer = 3;
+            System.out.println(primer);
+        short segundo = 5000;
+            System.out.println(segundo);
+        int tercer = 7000;
+            System.out.println(tercer);
+        long cuarto = 156184521;
+            System.out.println(cuarto);
+
+        //Datos Float (decimales)
+        float quinto = 35.57f;
+            System.out.println(quinto);
+        double sexto = 324.6574d;
+            System.out.println(sexto);
+
+        //Caracter
+        char caracter = 'A';
+            System.out.println(caracter);
     }
 }
