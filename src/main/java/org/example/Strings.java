ff22bb2ecad91d0b5f226a8c6e57d03d23f29ff5
Luis Aranguren <luisarangurnn@gmail.com>
2024-10-28 07:25:02 -0300

    feat: Strings01

---
+++ src/main/java/org/example/Strings.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Strings {
+
+    public static void main(String[] args){
+        String name = "Luis";
+
+        String surname = new String( "Aranguren");
+
+        String mensaje = "Mi nombre es " + name + " " + surname;
+
+        System.out.println(mensaje);
+
+    }
+}
