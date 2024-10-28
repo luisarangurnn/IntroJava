ab7d119d10be18258a2743fdbede11582347d045
Luis Aranguren <luisarangurnn@gmail.com>
2024-10-28 06:30:00 -0300

    feat: addBoolean

---
+++ src/main/java/org/example/PrimitiveTypes.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        //escribir un boolean, asignarlo y mostrar en consola
+        boolean isActive = true;
+
+        System.out.println(isActive);
+    }
+}
